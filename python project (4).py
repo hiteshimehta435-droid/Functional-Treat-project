f555bf71997eb07b94cd5ebb9a792536d77535ef
hiteshimehta435-droid <hiteshimehta435@gmail.com>
2026-06-25 17:56:38 +0530

    Add files via upload

---
+++ python project (4).py
@@ -0,0 +1,90 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+========================================== RESTART: E:\New folder (6)\python note\python wrok\Functional Treat project.py =========================================
+Welcome to Functional Treat Analyzer and Transformer
+1. user input data
+2. data summary
+3. lambda function
+4. sort data
+5. *args function
+6. **kwargs function
+7. exit
+Enter your choice: 1
+Enter numbers: 10 30 20 40 
+Data stored successfully: 
+Welcome to Functional Treat Analyzer and Transformer
+1. user input data
+2. data summary
+3. lambda function
+4. sort data
+5. *args function
+6. **kwargs function
+7. exit
+Enter your choice: 2
+
+Data summary
+Total values:  4
+Maximum values:  40
+Minimum values:  10
+sum : 100
+Average : 25.0
+Welcome to Functional Treat Analyzer and Transformer
+1. user input data
+2. data summary
+3. lambda function
+4. sort data
+5. *args function
+6. **kwargs function
+7. exit
+Enter your choice: 3
+Enter threshlode value: 20
+Filtered data: [30, 40]
+Welcome to Functional Treat Analyzer and Transformer
+1. user input data
+2. data summary
+3. lambda function
+4. sort data
+5. *args function
+6. **kwargs function
+7. exit
+Enter your choice: 4
+sorted data: [10, 20, 30, 40]
+Welcome to Functional Treat Analyzer and Transformer
+1. user input data
+2. data summary
+3. lambda function
+4. sort data
+5. *args function
+6. **kwargs function
+7. exit
+Enter your choice: 5
+Enter numbers: 10 20 40
+Total: 70
+Welcome to Functional Treat Analyzer and Transformer
+1. user input data
+2. data summary
+3. lambda function
+4. sort data
+5. *args function
+6. **kwargs function
+7. exit
+Enter your choice: 6
+name: Hiteshi
+course: AI / ML data science
+project: Functional Treat
+name : Hiteshi
+course : AI / ML data science
+project : Functional Treat
+Welcome to Functional Treat Analyzer and Transformer
+1. user input data
+2. data summary
+3. lambda function
+4. sort data
+5. *args function
+6. **kwargs function
+7. exit
+Enter your choice: 7
+Exit
+Thank you to use my project.
+Have nice day.
